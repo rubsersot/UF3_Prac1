8e12edf25aa1337f338a7459646f63a1f24e62ad
ausias <ausias@hINFO6-24.xiam.cat>
2023-01-30 10:17:58 +0100

    exer1

--- UF3_Prac1/src/uf3_prac1/Exer1.java
+++ UF3_Prac1/src/uf3_prac1/Exer1.java
@@ -4,6 +4,12 @@
  */
 package uf3_prac1;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author ausias
@@ -12,9 +18,24 @@ public class Exer1 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException{
+        Scanner scan = new Scanner(System.in);
+        File arxiu = new File("./textos.txt");
+        FileWriter writer = new FileWriter(arxiu, false);
+        PrintWriter pw = new PrintWriter(writer);
+        
+        String missatge = scan.nextLine();
+        while(!missatge.equals("")){
+            pw.println(missatge);
+            missatge = scan.nextLine();
+        }
+        
+        pw.flush();
+        writer.close();
+        
+        scan.close();
     }
     
 }
