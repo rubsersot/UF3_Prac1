b2a8f4ee88d07da69d2d6707ba19746594daf811
Pablo Jiménez <Pablo Jimnez@DESKTOP-UEAH07A>
2023-01-30 18:30:18 +0100

    Exercici2

--- UF3_Prac1/src/uf3_prac1/Exer2.java
+++ UF3_Prac1/src/uf3_prac1/Exer2.java
@@ -4,6 +4,12 @@
  */
 package uf3_prac1;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author ausias
@@ -12,9 +18,43 @@ public class Exer2 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException{
+        Scanner scan = new Scanner(System.in);
+        File arxiu = new File("./textos.txt");
+        FileWriter writer;
+        PrintWriter pw;
+        
+        // Comprobar si el archivo existe
+        if (arxiu.exists()) {
+            writer = new FileWriter(arxiu, true); // agregar al final
+        } else {
+            arxiu.createNewFile(); // crear nuevo archivo
+            writer = new FileWriter(arxiu, false);
+        }
+        pw = new PrintWriter(writer);
+        
+        String missatge = scan.nextLine();
+        while(!missatge.equals("")){
+            if(missatge.equals("@ESBORRA")){
+                pw.flush();
+                writer.close();
+                arxiu.delete();
+                arxiu.createNewFile();
+                writer = new FileWriter(arxiu, false);
+                pw = new PrintWriter(writer);
+            
+            }
+            else{
+                pw.println(missatge);
+            }
+            missatge = scan.nextLine();
+        }
+        
+        pw.flush();
+        writer.close();        
+        scan.close();
     }
     
 }
